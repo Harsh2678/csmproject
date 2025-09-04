102b4a032344c8d33fa9f5e47b01c0f8e7815c45
Harsh Shah <harsh.shah0812@gmail.com>
2025-09-04 14:32:09 +0530

    add sub category ui

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from cmsproject.models import Category, SubCategory
 
 def home(request):
-    return render(request, "home.html")
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, "home.html", context)
 
 def about(request):
     return render(request, "about.html")
+
+
+def subcategories(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    sub_categories = SubCategory.objects.filter(category=category).order_by('sub_category_name')
+    context = {
+        'category': category,
+        'sub_categories': sub_categories,
+    }
+    return render(request, "subcategories.html", context)
